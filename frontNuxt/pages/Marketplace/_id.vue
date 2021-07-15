3d9e9b38a995ef83fe76a78535efaaa6d7ee8ab0
ManuYuzu <manuaglez@gmail.com>
2021-07-15 11:24:21 +0100

    erase product w/ with alert

--- frontNuxt/pages/Marketplace/_id.vue
+++ frontNuxt/pages/Marketplace/_id.vue
@@ -2,11 +2,19 @@
   <div v-if="product">
     <div class="productpagebody">
       <v-banner class="pa-5 pt-0">
-        <h2>{{ product.name.toUpperCase() }}</h2>
+        <v-row>
+          <v-col
+            ><h2>{{ product.name.toUpperCase() }}</h2></v-col
+          >
+          <v-col
+            ><v-btn @click="deleteoverlay = !deleteoverlay"
+              >Borrar Producto</v-btn
+            ></v-col
+          >
+        </v-row>
       </v-banner>
       <v-row class="pa-15 d-flex align-center">
-
-        <v-col cols="12" md="6" v-if="product">
+        <v-col v-if="product" cols="12" md="6">
           <ProductImage :product="product" @click.native="overlay = !overlay" />
         </v-col>
         <v-col cols="12" md="6">
@@ -19,6 +27,7 @@
         </v-col>
       </v-row> -->
       <v-divider></v-divider>
+      <!-- OVERLAY TO SHOW FULL IMAGE -->
       <v-overlay :absolute="absolute" :value="overlay">
         <v-img
           :src="product.image[0]"
@@ -27,6 +36,28 @@
           @click="overlay = false"
         >
         </v-img>
+      </v-overlay>
+
+      <!-- OVERLAY TO DELETE PRODUCTS -->
+      <v-overlay :absolute="absolute" :value="deleteoverlay">
+        <v-card>
+          <v-card-title> ¡Atención! </v-card-title>
+          <v-card-text>
+            <span
+              >¿Estás seguro de querer borrar este producto? Se borrarán todos
+              los datos de este producto y no podrás recuperarlos.</span
+            >
+          </v-card-text>
+          <v-card-actions>
+            <v-btn @click="deleteProductPage" dark color="light-blue lighten-2">
+              Borrar producto
+            </v-btn>
+            <v-btn @click="deleteoverlay = !deleteoverlay" dark color="light-blue lighten-2">
+              Volver
+            </v-btn>
+            
+          </v-card-actions>
+        </v-card>
       </v-overlay>
     </div>
   </div>
@@ -45,10 +76,22 @@ export default {
     return {
       absolute: true,
       overlay: false,
-   
+      deleteoverlay: false
     }
   },
-
+  methods: {
+    async deleteProductPage() {
+      console.log('----------', this.product)
+      const response = await this.$axios.$delete(`/products/${this.product._id}`, 
+      {
+        headers: {
+        token: localStorage.token
+        }
+      })
+     this.$router.push({ name: 'Marketplace' })
+     return response
+    }
+  },
 }
 </script>
 
