e5029f21b7e4085f1d24359bffe18350acc0af65
ManuYuzu <manuaglez@gmail.com>
2021-07-21 15:19:44 +0100

    added .env to run

--- frontNuxt/pages/index.vue
+++ frontNuxt/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-app-bar height="920" flat prominent src="https://static-cse.canva.com/blob/215565/beautiful-twitter-banners.jpg">
+    <v-app-bar id="custombar" height="920" flat prominent src="https://static-cse.canva.com/blob/215565/beautiful-twitter-banners.jpg">
     </v-app-bar>
     <v-container fluid>
         <div class="d-flex flex-wrap flex-row justify-center mx-5" >
@@ -34,3 +34,10 @@ export default {
 };
 </script>
 
+<style scoped>
+
+#custombar {
+  border-bottom: 41% ;
+}
+
+</style>
